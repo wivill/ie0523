76066bb064faa7fc06ec73416a7ee82cb9cf4858
Jose <jose.andres.mm.04@gmail.com>
2018-05-12 13:58:33 -0600

    Se creo el archivo de byte joining con sus entradas y salidas

---
+++ proyecto-01/bytejoining.v
@@ -0,0 +1,16 @@
+
+
+
+
+
+module byte_joining (
+  input [7:0] Lane_0;
+  input [7:0] Lane_1;
+  input [7:0] Lane_2;
+  input [7:0] Lane_3;
+  input [1:0] ctr_3;
+  output [7:0] out;
+
+  );
+
+endmodule //byte_joining
