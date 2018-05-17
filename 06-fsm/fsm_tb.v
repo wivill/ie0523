2b2d5d52be8d9cfa7fbe8d52f0dcc0d72a85de46
Willy <gopwivill@gmail.com>
2018-05-17 14:07:33 -0600

    yosys scripts tarea 06, readme y pequeños ajustes. Pendiente tester

--- 06-fsm/fsm_tb.v
+++ 06-fsm/fsm_tb.v
@@ -12,8 +12,8 @@ module testbench;
 
    // Wires para alambrar módulos
    wire CLK_tb, go_up_tb, go_down_tb, halt_tb;
-   wire top_lim_struct_tb, top_lim_cond_tb, top_lim_delay_tb;
-   wire bott_lim_struct_tb, bott_lim_cond_tb, bott_lim_delay_tb;
+   wire top_lim_synth_tb, top_lim_cond_tb, top_lim_synth_delay_tb;
+   wire bott_lim_synth_tb, bott_lim_cond_tb, bott_lim_synth_delay_tb;
 
    tester     letest( .CLK             (CLK_tb),
                       .go_down         (go_down_tb),
@@ -21,10 +21,10 @@ module testbench;
                       .halt            (halt_tb),
                       .top_lim_cond    (top_lim_cond_tb),
                       .bott_lim_cond   (bott_lim_cond_tb),
-                      .top_lim_struct  (top_lim_struct_tb),
-                      .bott_lim_struct (bott_lim_struct_tb),
-                      .top_lim_delay   (top_lim_delay_tb),
-                      .bott_lim_delay  (bott_lim_delay_tb),
+                      .top_lim_synth  (top_lim_synth_tb),
+                      .bott_lim_synth (bott_lim_synth_tb),
+                      .top_lim_synth_delay   (top_lim_synth_delay_tb),
+                      .bott_lim_synth_delay  (bott_lim_synth_delay_tb),
                       );
 
   ascensor_cond         a_cond(   .CLK        (CLK_tb),
@@ -35,28 +35,28 @@ module testbench;
                                   .bott_lim   (bott_lim_cond_tb),
                               );
 
-  ascensor_yosys      a_yosys (   .CLK        (CLK_tb),
+  ascensor_synth      a_synth (   .CLK        (CLK_tb),
                                   .go_down    (go_down_tb),
                                   .go_up      (go_up_tb),
                                   .halt       (halt_tb),
-                                  .top_lim    (top_lim_struct_tb),
-                                  .bott_lim   (bott_lim_struct_tb),
+                                  .top_lim    (top_lim_synth_tb),
+                                  .bott_lim   (bott_lim_synth_tb),
                                   );
 
-  ascensor_yosys_delay a_delay(   .CLK        (CLK_tb),
+  ascensor_synth_delay a_synth_delay(   .CLK        (CLK_tb),
                                   .go_down    (go_down_tb),
                                   .go_up      (go_up_tb),
                                   .halt       (halt_tb),
-                                  .top_lim    (top_lim_delay_tb),
-                                  .bott_lim   (bott_lim_delay_tb),
+                                  .top_lim    (top_lim_synth_delay_tb),
+                                  .bott_lim   (bott_lim_synth_delay_tb),
                                   );
 
   verifier          leverifier(   .top_lim_cond    (top_lim_cond_tb),
                                   .bott_lim_cond   (bott_lim_cond_tb),
-                                  .top_lim_struct  (top_lim_struct_tb),
-                                  .bott_lim_struct (bott_lim_struct_tb),
-                                  .top_lim_delay   (top_lim_delay_tb),
-                                  .bott_lim_delay  (bott_lim_delay_tb),
+                                  .top_lim_synth  (top_lim_synth_tb),
+                                  .bott_lim_synth (bott_lim_synth_tb),
+                                  .top_lim_synth_delay   (top_lim_synth_delay_tb),
+                                  .bott_lim_synth_delay  (bott_lim_synth_delay_tb),
                                   );
 
 endmodule
