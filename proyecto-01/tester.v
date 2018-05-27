44951cd13b36fe2d6f2f1732edc3269c6ff80e6b
Willy <gopwivill@gmail.com>
2018-05-27 16:28:13 -0600

    alambrado final

--- proyecto-01/tester.v
+++ proyecto-01/tester.v
@@ -1,27 +1,14 @@
 `timescale 1ns / 100ps
 
-`include "../s2p/freq_div_cond.v"
-
-module tester_s2p(  output reg          CLK_2MHz,
-                    output              CLK_1MHz,
-                    output              CLK_500KHz,
-                    output              CLK_250KHz,
+module tester(  output reg  [7:0]   IN_COM, IN_PAD, IN_SKP, IN_STP, IN_SDP, IN_END, IN_EDB, IN_FTS, IN_IDL, IN_TLP,
+                    output reg          CLK_2MHz,
+                    output reg  [3:0]   IN_CTRL,
                     output reg          ENB,
-                    output reg          reset,
-                    // output reg  [2:0]   CTR,
-                    output reg  [31:0]  DATO,
-                    output reg          VALID
+                    output reg          reset
                     );
 
-  freq_div_cond clocks_cond ( .CLK    (CLK_2MHz),
-                              .reset  (reset),
-                              .CLK_2  (CLK_1MHz),
-                              .CLK_4  (CLK_500KHz),
-                              .CLK_8  (CLK_250KHz)
-                              );
-
   initial begin
-    $dumpfile("p2s_s2p.vcd");
+    $dumpfile("pcie.vcd");
     $dumpvars;
   end
 
@@ -34,17 +21,12 @@ module tester_s2p(  output reg          CLK_2MHz,
     repeat(2) #500 CLK_2MHz = ~CLK_2MHz;
     ENB = 1'b1;
     reset = 1'b1;
-    VALID = 1'b1;
     forever #500 CLK_2MHz = ~CLK_2MHz;
   end
 
-  // always @ (posedge CLK_2MHz) begin
-  //   CTR = CTR + 1;
-  // end
-
   initial begin
     @(posedge ENB)
-    repeat(8) @(posedge CLK_2MHz);
+    // repeat(8) @(posedge CLK_2MHz);
     DATO = 32'h01234567;
     repeat(8) @(posedge CLK_2MHz);
     DATO = 32'h89ABCDEF;
