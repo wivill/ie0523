ae6228edacfb753d29430c02af37a975164be23b
wivill <gopwivill@gmail.com>
2018-06-25 17:39:37 -0600

    wrr completo sin probar

--- proyecto-02/wrr/wrr_cond.v
+++ proyecto-02/wrr/wrr_cond.v
@@ -6,4 +6,29 @@ module wrr_cond(  input   reset,
 
                   );
 // hacer tabla de VC y pesos para que lo lea wrr_cond y asignar así el canal en el mux.
+
+  reg [1:0] current_request_id;
+  reg [2:0] weight_ctr, current_weight;
+
+
+  always @ (~reset) begin
+    grant_id <= 2'b00;
+    current_request_id <= 2'b00;
+    weight_ctr <= 3'b111;
+    current_weight <= 3'b000;
+  end
+
+  always @ (posedge clk) begin
+    weight_ctr <= weight_ctr + 1'b1;
+
+      if (current_weight == weight_ctr) begin
+        weight_ctr <= 3'b111;
+        current_request_id <= request_id;
+        current_weight <= weight;
+      end else begin
+        grant_id <= current_request_id;
+      end
+    end
+
+
 endmodule
