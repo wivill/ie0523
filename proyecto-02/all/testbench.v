59f220d2b5dc4c07c358e3bbc572ff3c1023558f
wivill <gopwivill@gmail.com>
2018-06-30 23:34:28 -0600

    testbench all init

--- proyecto-02/all/testbench.v
+++ proyecto-02/all/testbench.v
@@ -7,34 +7,24 @@
 
 module testbench;
 
-wire sReset;
-wire sInit;
-wire [3:0] sEmpty;
-wire [3:0] sFull;
-wire [3:0] sPause;
-wire [3:0] sContinue;
-
-wire oInit;
-wire oIdle;
-wire [3:0] oError;
-wire [3:0] stbPause;
-wire [3:0] stbContinue;
-wire [7:0] State, nState;
-
-
-
-  wire [3:0] Data_Word_tb;
-  wire       clk_tb, reset_tb, edit_weight_tb;
-  wire [1:0] vc_assign_tb;
-  wire [2:0] weight_assign_tb;
-
-  wire [1:0] VC_id_cond_tb, request_id_cond_tb;
-  wire [2:0] weight_cond_tb;
-  wire       data_out_cond_tb;
-
-  wire [1:0] VC_id_synth_tb, request_id_synth_tb;
-  wire [2:0] weight_synth_tb;
-  wire       data_out_synth_tb;
+  wire       clk,
+  wire       enb,
+  wire       reset,
+  wire [3:0] Data_Word,
+  wire       edit_weight,
+  wire [1:0] vc_assign,
+  wire [2:0] weight_assign,
+  wire       sInit,
+  // wire FIFOs
+  wire [3:0] lectura, escritura,
+  wire [2:0] umbralVacio0, umbralLleno0,
+  wire [2:0] umbralVacio1, umbralLleno1,
+  wire [2:0] umbralVacio2, umbralLleno2,
+  wire [2:0] umbralVacio3, umbralLleno3,
+  wire      oInit,
+  wire      oIdle,
+  wire [3:0]oError, stbPause, stbContinue,
+  wire [7:0]State, nState
 
   tester  letest( .weight_assign  (weight_assign_tb),
                   .vc_assign      (vc_assign_tb),
@@ -43,10 +33,10 @@ wire [7:0] State, nState;
                   .edit_weight    (edit_weight_tb),
                   .Data_Word      (Data_Word_tb)
   );
-// conditional
-
-// synth
-  
+  // conditional
+  qos_cond conditional();
+  // synth
+  qos_synth synthetized();
 
 
 endmodule
